84538e48aa62fe851b8a34f37cda2619f312c59e
John <jlobblet-github@gmx.co.uk>
2020-06-24 08:24:44 +0000

    Add DynamoDB setup files

---
+++ setup/dynamodb.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import json
+
+import boto3
+
+param_loc = "friendly_name_table.json"
+
+if __name__ == "__main__":
+    dynamodb = boto3.resource("dynamodb")
+
+    print(f"Loading table parameters from {param_loc}...")
+    with open(param_loc, "r") as json_file:
+        table_params = json.loads(json_file.readlines())
+    print("...loaded")
+
+    try:
+        print("Creating new table...")
+        table = dynamodb.create_table(**table_params)
+        table.meta.client.get_waiter("table_exists").wait(
+            TableName=table_params["TableName"]
+        )
+        print("...done")
+    except dynamodb.exceptions.ResourceInUseException as e:
+        print(f"ResourceInUseException:\n{e}")
+else:
+    print("Trying to run dynamodb setup not as __main__, exiting.")
