831db3d697c24a83d9705b0b7cf20856b468bb41
John <jlobblet-github@gmx.co.uk>
2020-06-24 08:46:53 +0000

    Update DynamoDB setup to work
    Fix incorrect type of read JSON data.
    Remove try/except block.

--- setup/dynamodb.py
+++ setup/dynamodb.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import json
 
+import botocore
 import boto3
 
 param_loc = "friendly_name_table.json"
@@ -10,17 +11,14 @@ if __name__ == "__main__":
 
     print(f"Loading table parameters from {param_loc}...")
     with open(param_loc, "r") as json_file:
-        table_params = json.loads(json_file.readlines())
+        table_params = json.loads(json_file.read())
     print("...loaded")
 
-    try:
-        print("Creating new table...")
-        table = dynamodb.create_table(**table_params)
-        table.meta.client.get_waiter("table_exists").wait(
-            TableName=table_params["TableName"]
-        )
-        print("...done")
-    except dynamodb.exceptions.ResourceInUseException as e:
-        print(f"ResourceInUseException:\n{e}")
+    print("Creating new table...")
+    table = dynamodb.create_table(**table_params)
+    table.meta.client.get_waiter("table_exists").wait(
+        TableName=table_params["TableName"]
+    )
+    print("...done")
 else:
     print("Trying to run dynamodb setup not as __main__, exiting.")
