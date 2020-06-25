4e717d3d945b6b2842f8772ab11356eed3b57f2e
John <jlobblet-github@gmx.co.uk>
2020-06-25 12:37:31 +0000

    Update queries with new commands
    Add and remove now use the new check.
    Add validates the instance can be found.
    Describe now gives some information about the instance.
    Show now prints information more nicely.

--- commands/queries.py
+++ commands/queries.py
@@ -1,3 +1,5 @@
+import datetime
+
 import botocore
 import boto3
 from boto3.dynamodb.conditions import Key
@@ -6,7 +8,7 @@ from discord.ext.commands import Context, Bot
 
 from config import CONFIG
 from helptexts import HELPTEXTS
-from utils.utils import user_is_me
+from utils.utils import get_instance_from_name, user_is_me
 
 
 class Queries(commands.Cog):
@@ -21,23 +23,47 @@ class Queries(commands.Cog):
         response = self.table.query(
             KeyConditionExpression=Key("GuildID").eq(str(ctx.guild.id))
         )
-        await ctx.send(response["Items"])
+        msg = "\n".join(
+            (
+                "{}: {}".format(x["FriendlyName"], x["InstanceID"])
+                for x in response["Items"]
+            )
+        )
+        await ctx.send(msg)
 
     @commands.command(
         name="describe", help=HELPTEXTS.DESCRIBE.full, brief=HELPTEXTS.DESCRIBE.brief
     )
     async def describe(self, ctx: Context, FriendlyName):
-        pass
+        instance = get_instance_from_name(ctx, FriendlyName, self.ec2, self.table)
+        instance.load()
+        state = instance.state["Name"]
+        time = datetime.datetime.now().replace(second=0) - instance.launch_time.replace(
+            tzinfo=None, second=0
+        )
+
+        await ctx.send(f"{FriendlyName} is currently {state}.")
+        if state == "running":
+            await ctx.send(
+                f"It has been running since {instance.launch_time}, for a duration of {time}."
+            )
+        elif state == "stopped":
+            await ctx.send(f"It was last started {time} ago.")
 
     @commands.check(user_is_me)
     @commands.command(name="add", help=HELPTEXTS.ADD.full, brief=HELPTEXTS.ADD.brief)
     async def add(self, ctx: Context, FriendlyName, InstanceID):
-        Item = {
-            "GuildID": str(ctx.guild.id),
-            "FriendlyName": FriendlyName,
-            "InstanceID": InstanceID,
-        }
-        self.table.put_item(Item=Item)
+        try:
+            instance = self.ec2.Instance(InstanceID)
+            instance.load()
+            Item = {
+                "GuildID": str(ctx.guild.id),
+                "FriendlyName": FriendlyName,
+                "InstanceID": InstanceID,
+            }
+            self.table.put_item(Item=Item)
+        except botocore.exceptions.ClientError as error:
+            await ctx.send(f"ClientError:\n{error}")
 
     @commands.check(user_is_me)
     @commands.command(
