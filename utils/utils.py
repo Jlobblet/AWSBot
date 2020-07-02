f8c06e8328b9f9a3066392e21cef9d1109c5ee11
John <jlobblet-github@gmx.co.uk>
2020-07-02 14:08:04 +0000

    Streamline creation of resources when not given

--- utils/utils.py
+++ utils/utils.py
@@ -7,10 +7,8 @@ from config import CONFIG
 
 
 def get_instance_from_name(ctx, instance_name, ec2=None, table=None):
-    if not ec2:
-        ec2 = boto3.resource("ec2")
-    if not table:
-        table = boto3.resource("dynamodb").Table(CONFIG.table)
+    ec2 = ec2 or boto3.resource("ec2")
+    table = table or boto3.resource("dynamodb").Table(CONFIG.table)
     item = table.get_item(
         Key={"GuildID": str(ctx.guild.id), "FriendlyName": instance_name}
     )["Item"]
